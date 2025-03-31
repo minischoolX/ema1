touchapp/openedx-app-android

fix: issue to load the video through chrome-cast device (#83)
fixes: LEARNER-10350

course/src/main/java/org/openedx/course/presentation/unit/video/EncodedVideoUnitViewModel.kt
@@ -120,6 +120,7 @@ class EncodedVideoUnitViewModel(
         val executor = Executors.newSingleThreadExecutor()
         CastContext.getSharedInstance(context, executor).addOnSuccessListener { castContext ->
             castPlayer = CastPlayer(castContext)
+            _isUpdated.value = true
         }.addOnFailureListener {
             logger.e(it, true)
         }

minischoolX/ema1

course/src/main/java/org/openedx/course/presentation/unit/video/EncodedVideoUnitViewModel.kt
@@ -106,6 +106,7 @@ class EncodedVideoUnitViewModel(
         CastContext.getSharedInstance(context, executor).addOnCompleteListener {
             it.result?.let { castContext ->
                 castPlayer = CastPlayer(castContext)
+                isUpdatedMutable.value = true
             }
         }
     }